1b983c6b0ddc039d8b5582c93d313b3c22818ce8
Maria Chapman <maria.chapman@thetrainline.com>
2019-08-21 13:02:11 +0100

    Adding lizardSpock

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-const validInput = ['rock', 'paper', 'scissors'];
+const RPSvalidInput = ['rock', 'paper', 'scissors'];
 // x beats y
-const matchups = {
-  rock: ['scissors'],
-  scissors: ['paper'],
-  paper: ['rock']
-}
+const RPSmatchups = {
+  rock: ['blunts scissors'],
+  scissors: ['cut paper'],
+  paper: ['covers rock']
+};
+
+const RPSLSvalidInput = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const RPSLSmatchups = {
+  rock: ['blunts scissors', 'crushes lizard'],
+  scissors: ['cut paper', 'decapitates lizard'],
+  paper: ['covers rock', 'disproves spock'],
+  lizard: ['poisons spock', 'eats paper'],
+  spock: ['vaporizes rock', 'smashes scissors']
+};
+
 const normaliseInput = input => typeof input === 'string' ? input.toLowerCase() : null;
+const findMatchup = (p1, p2, matchups) => {
+  const p1Winner = matchups[p1].find(s => s.includes(p2));
+  return p1Winner ? {
+    text: `${p1} ${p1Winner}`,
+    winningPlayer: '1'
+  } : {
+      text: `${p2} ${matchups[p2].find(s => s.includes(p1))}`,
+      winningPlayer: '2'
+    };
+}
+
+const play = (validInputs, matchups) => (player1input, player2input) => {
+  const p1normalisedInput = normaliseInput(player1input);
+  const p2normalisedInput = normaliseInput(player2input);
+
+  if (!validInputs.includes(p1normalisedInput) || !validInputs.includes(p2normalisedInput)) {
+    return `Please pick from: ${validInputs.join(', ')}`;
+  }
+
+  if (p1normalisedInput === p2normalisedInput) {
+    return 'Draw!';
+  }
+
+  const { text, winningPlayer } = findMatchup(p1normalisedInput, p2normalisedInput, matchups);
+  console.log(text)
+
+  return `Player ${winningPlayer} wins! ${text}.`;
+};
 
 module.exports = {
-  rockPaperScissors: (player1input, player2input) => {
-    const p1normalisedInput = normaliseInput(player1input);
-    const p2normalisedInput = normaliseInput(player2input);
-
-    if (!validInput.includes(p1normalisedInput) || !validInput.includes(p2normalisedInput)) {
-      return `Please pick from: ${validInput.join(', ')}`;
-    }
-
-    if (p1normalisedInput === p2normalisedInput) {
-      return 'Draw!';
-    }
-
-    return matchups[p1normalisedInput].includes(p2normalisedInput)
-      ? 'Player 1 wins!'
-      : 'Player 2 wins!';
-  }
+  rockPaperScissors: play(RPSvalidInput, RPSmatchups),
+  lizardSpock: play(RPSLSvalidInput, RPSLSmatchups)
 };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { rockPaperScissors } = require('./index');
+const { rockPaperScissors, lizardSpock } = require('./index');
 
 describe('rockPaperScissors', () => {
   it('should exist', () => {
@@ -8,18 +8,48 @@ describe('rockPaperScissors', () => {
   it.each([
     ['rock', 'rock', 'Draw!'],
     ['Rock', 'ROCK', 'Draw!'],
-    ['rock', 'scissors', 'Player 1 wins!'],
-    ['scissors', 'rock', 'Player 2 wins!'],
-    ['SCIssors', 'paper', 'Player 1 wins!'],
-    ['paper', 'scissors', 'Player 2 wins!'],
-    ['paper', 'rock', 'Player 1 wins!'],
-    ['rock', 'paper', 'Player 2 wins!'],
+    ['rock', 'scissors', 'Player 1 wins! rock blunts scissors.'],
+    ['scissors', 'rock', 'Player 2 wins! rock blunts scissors.'],
+    ['SCIssors', 'paper', 'Player 1 wins! scissors cut paper.'],
+    ['paper', 'scissors', 'Player 2 wins! scissors cut paper.'],
+    ['paper', 'rock', 'Player 1 wins! paper covers rock.'],
+    ['rock', 'paper', 'Player 2 wins! paper covers rock.'],
     ['mega shark', 'giant octopus', 'Please pick from: rock, paper, scissors'],
     ['rock', undefined, 'Please pick from: rock, paper, scissors']
   ])(
     'should return the correct output for %s vs %s',
     (player1, player2, expected) => {
       expect(rockPaperScissors(player1, player2)).toBe(expected);
+    }
+  )
+});
+
+describe('lizardSpock', () => {
+  it('should exist', () => {
+    expect(lizardSpock).toBeTruthy();
+  });
+
+  it.each([
+    ['rock', 'rock', 'Draw!'],
+    ['Rock', 'ROCK', 'Draw!'],
+    ['rock', 'scissors', 'Player 1 wins! rock blunts scissors.'],
+    ['scissors', 'rock', 'Player 2 wins! rock blunts scissors.'],
+    ['SCIssors', 'paper', 'Player 1 wins! scissors cut paper.'],
+    ['paper', 'scissors', 'Player 2 wins! scissors cut paper.'],
+    ['paper', 'rock', 'Player 1 wins! paper covers rock.'],
+    ['rock', 'paper', 'Player 2 wins! paper covers rock.'],
+    ['spock', 'rock', 'Player 1 wins! spock vaporizes rock.'],
+    ['rock', 'spock', 'Player 2 wins! spock vaporizes rock.'],
+    ['spock', 'lizard', 'Player 2 wins! lizard poisons spock.'],
+    ['lizard', 'spock', 'Player 1 wins! lizard poisons spock.'],
+    ['scissors', 'lizard', 'Player 1 wins! scissors decapitates lizard.'],
+    ['lizard', 'scissors', 'Player 2 wins! scissors decapitates lizard.'],
+    ['mega shark', 'giant octopus', 'Please pick from: rock, paper, scissors, lizard, spock'],
+    ['rock', undefined, 'Please pick from: rock, paper, scissors, lizard, spock']
+  ])(
+    'should return the correct output for %s vs %s',
+    (player1, player2, expected) => {
+      expect(lizardSpock(player1, player2)).toBe(expected);
     }
   )
 });
